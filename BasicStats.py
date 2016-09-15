1afbc1c361f8a6c887c9dc55a301c77b7597efea
fuxing4203 <fuxing4203@gmail.com>
2016-09-15 17:18:38 -0400

    Update BasicStats.py

--- BasicStats.py
+++ BasicStats.py
@@ -2,12 +2,14 @@ from Document import Document
 from Sentence import Sentence
 from DocumentStream import DocumentStream
 
-
-
 class BasicStats:
-
     @staticmethod
     def createFreqMap(wlist):
+        '''
+        create a frequency map of a list of words by using dictionary
+        if word already in dictionary, frequency + 1
+        else create another tag if the words is not empty
+        '''
         b = wlist
         adict = {}
         for word in b:
@@ -15,11 +17,13 @@ class BasicStats:
                 adict[word.lower()] += 1
             elif word.lower() not in adict and word != '':
                 adict[word.lower()] = 1
-        
-            
         return adict
+        
     @staticmethod
     def maxl(kdict):
+        '''
+        helper function for topN
+        '''
         b = 0
         s = ''
         for i in kdict:
@@ -33,7 +37,6 @@ class BasicStats:
     def topN(self, bdict, n):
         cdict = {}
         for i in range(n):
-
             cdict[self.maxl(bdict)] = bdict[self.maxl(bdict)]
             bdict[self.maxl(bdict)] = 1
         return cdict
