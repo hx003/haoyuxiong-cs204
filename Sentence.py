ad754f16e64787c28255ec1891ebec1f9f17d74e
fuxing4203 <fuxing4203@gmail.com>
2016-09-15 17:24:26 -0400

    Update Sentence.py

--- Sentence.py
+++ Sentence.py
@@ -1,20 +1,31 @@
 class Sentence:
 
     def __init__(self, sen):
+        '''
+        initiate variables
+        punctuation is the last letter in the element
+        sen is the list of sentence created by DocumentStream
+        '''
         self.wordcount = 0
         self.charcount = 0
         self.sen = sen
-        self.puncutaion = sen[-1]
+        self.punctuation = sen[-1]
 
         
     def parseWords(self):
-
-
+        '''
+        wordlist is created by splitting the sentence with space
+        self.wordcount is updated
+        '''
         wordlist = self.sen[:-1].split()
         self.wordcount = len(wordlist)
         return wordlist
 
     def parseChar(self):
+        '''
+        extract every character from self.sen to a list
+        counted by length and stored in self.charcount
+        '''
         charlist = [i for i in self.sen]
         self.charcount = len(charcount)
         return charlist
