8fc64cf187ff02584dde8c02bacda487b8d253da
fuxing4203 <fuxing4203@gmail.com>
2016-09-26 18:25:05 -0400

    Add files via upload

--- MatPlotPloter.py
+++ MatPlotPloter.py
@@ -1,6 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class MatPlotPloter:
-    import matplotlib.pyplot as plt
-    import numpy as np
-    def scatterPlot(x, y):
-        plt.scatter(x, y)
-        plt.show
+    def __init__(self):
+        self.figure = 1
+        
+    def scatterPlot(self, x, y):
+        plt.figure(self.figure)
+        self.figure += 1
+        colors = np.random.rand(len(y))
+        size = [10 * np.pi * i ** 2 for i in y]
+        plt.scatter(x, y, s = size, c = colors, alpha = 0.5)
+        plt.show()
+        
+    def barGraph(self, x, y):
+        plt.figure(self.figure)
+        self.figure += 1
+        plt.barh(y, x)
+        plt.show()
