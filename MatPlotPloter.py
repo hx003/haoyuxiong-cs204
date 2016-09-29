b744699254b5b49a65c9b1ad7f714244611acb4d
fuxing4203 <fuxing4203@gmail.com>
2016-09-28 21:08:02 -0400

    Update MatPlotPloter.py

--- MatPlotPloter.py
+++ MatPlotPloter.py
@@ -3,9 +3,16 @@ import numpy as np
 
 class MatPlotPloter:
     def __init__(self):
+        """
+        keep track of the figure number so that graphs won't be printed on the same figure
+        """
         self.figure = 1
         
     def scatterPlot(self, x, y):
+        """
+        do a scatter plot with two lists x and y
+        where the size of the bubble is related to the y value
+        """
         plt.figure(self.figure)
         self.figure += 1
         colors = np.random.rand(len(y))
@@ -14,6 +21,10 @@ class MatPlotPloter:
         plt.show()
         
     def barGraph(self, x, y):
+        """
+        a horizontal bar graph just like in command line plotter
+        takes in two lists x and y
+        """
         plt.figure(self.figure)
         self.figure += 1
         plt.barh(y, x)
