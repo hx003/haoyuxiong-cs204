35b62c8d02262df4192dede4d1ff62937c24c5c5
fuxing4203 <fuxing4203@gmail.com>
2016-10-20 16:15:41 -0400

    Add files via upload

--- TextFilter.py
+++ TextFilter.py
@@ -1,8 +1,12 @@
+from Document import Document
 class TextFilter:
     def __init__(self, D):
         #D is an object created by initializing Document
-        self.wordlist = D.wordlist
+        self.wordlist = []
         self.text = ''
+        for i in range(len(D)):
+            #wordlist is derived from the sentence list of the document
+            self.wordlist.extend(D.getIndexSlist(i).split())
         
     def nSpace(self):
         #nomalizes the white space
@@ -26,9 +30,9 @@ class TextFilter:
                 #ord 60 - 71: 1-9
                 #ord 101 - 132: Capital Letters
                 #ord 141 - 172: lower case letters
-                if (ord(a[i][ch]) >= 60 and ord(a[i][ch]) <= 71)) or \
-                   (ord(a[i][ch]) >= 101 and ord(a[i][ch]) <= 132)) or \
-                   (ord(a[i][ch]) >= 141 and ord(a[i][ch]) <= 172)):
+                if (ord(a[i][ch]) >= 60 and ord(a[i][ch]) <= 71) or \
+                   (ord(a[i][ch]) >= 101 and ord(a[i][ch]) <= 132) or \
+                   (ord(a[i][ch]) >= 141 and ord(a[i][ch]) <= 172):
                     word += a[i][ch]
             self.wordlist[i] = word
         
@@ -42,6 +46,10 @@ class TextFilter:
             self.wordlist[i] = word
 
     def apply(stringList):
+        '''
+        apply methods and piece back words into a text
+        return the text
+        '''
         #apply the methods
         while len(stringList) > 0:
             if stringList[0] == 'NWS':
@@ -61,6 +69,6 @@ class TextFilter:
             stringList = stringList[1:]
         for i in range(len(self.wordlist)):
             self.text += self.wordlist[i] + ' '
-                
+        return self.text       
 
         
