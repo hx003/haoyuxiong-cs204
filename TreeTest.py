94677bd9a078bc33b4af90dcbac17292c1a7519c
fuxing4203 <fuxing4203@gmail.com>
2016-11-06 18:45:16 -0500

    Update TreeTest.py

--- TreeTest.py
+++ TreeTest.py
@@ -18,3 +18,7 @@ def writeFile(text, author):
     file.write(text)
     file.close()
 
+'''
+['Author', 'Name', 'Genre', 'Year', 'Topics']
+[['James Joyce', 'Ulyssess', 'Novel', 1922, 'Modernist Novel'], ['James Joyce',	'AfterTheRace',	'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Araby', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Encounter', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Eveline	Short Story', 1904, 'Realist Fiction'], ['James Joyce', 'TheBoardingHouse', 'Short Story', 1914, 'Realist Fiction'], ['Mark Twain', 'ConnecticutYankee', 'Novel', 1889, 'Science Fiction'], ['Poe', 'CaskofAmontillado', 'Short Story', 1846, 'Horror'], ['Poe', 'FallHouseOfUsher', 'Short Story', 1839, 'Horror'], ['Poe', 'MasqueofTheRedDeath', 'Short Story', 1842, 'Horror'], ['Poe', 'Raven', 'Short Story', 1845, 'Horror']]
+'''
