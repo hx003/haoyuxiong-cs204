7f110a6bd61099332501786f228b3639fc38c5c4
fuxing4203 <fuxing4203@gmail.com>
2016-11-06 19:00:04 -0500

    Update TreeTest.py

--- TreeTest.py
+++ TreeTest.py
@@ -19,6 +19,6 @@ def writeFile(text, author):
     file.close()
 
 '''
-['Author', 'Name', 'Genre', 'Year', 'Topics']
-[['James Joyce', 'Ulyssess', 'Novel', 1922, 'Modernist Novel'], ['James Joyce',	'AfterTheRace',	'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Araby', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Encounter', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Eveline	Short Story', 1904, 'Realist Fiction'], ['James Joyce', 'TheBoardingHouse', 'Short Story', 1914, 'Realist Fiction'], ['Mark Twain', 'ConnecticutYankee', 'Novel', 1889, 'Science Fiction'], ['Poe', 'CaskofAmontillado', 'Short Story', 1846, 'Horror'], ['Poe', 'FallHouseOfUsher', 'Short Story', 1839, 'Horror'], ['Poe', 'MasqueofTheRedDeath', 'Short Story', 1842, 'Horror'], ['Poe', 'Raven', 'Short Story', 1845, 'Horror']]
+['Author', 'Genre', 'Year', 'Topics']
+[['James Joyce', 'Novel', 1922, 'Modernist Novel'], ['James Joyce', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Short Story', 1914, 'Realist Fiction'], ['James Joyce', 'Short Story', 1904, 'Realist Fiction'], ['James Joyce', 'Short Story', 1914, 'Realist Fiction'], ['Mark Twain', 'Novel', 1889, 'Science Fiction'], ['Poe', 'Short Story', 1846, 'Horror'], ['Poe', 'Short Story', 1839, 'Horror'], ['Poe', 'Short Story', 1842, 'Horror'], ['Poe', 'Short Story', 1845, 'Horror']]
 '''
